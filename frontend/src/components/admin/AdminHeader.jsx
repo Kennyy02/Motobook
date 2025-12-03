8a3b401fccf9a7cefb0334baec6736e63fdab0fe
alegaemman-glitch <alegaemmann@gmail.com>
2025-12-03 22:01:36 +0800

    fix the admin style

--- frontend/src/components/admin/AdminHeader.jsx
+++ frontend/src/components/admin/AdminHeader.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import "../../styles/admin/AdminHeader.css";
 
 function AdminHeader() {
-  <header className="admin-header">
-    <h1>Admin Dashboard</h1>
-  </header>;
+  return (
+    <header className="admin-header">
+      <h1>Admin Dashboard</h1>
+    </header>
+  );
 }
 
 export default AdminHeader;
