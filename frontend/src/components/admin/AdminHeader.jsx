cdb288e626782e31709edcc15abd4c5608b17393
alegaemman-glitch <alegaemmann@gmail.com>
2025-12-03 22:29:42 +0800

    fix the admin bg

--- frontend/src/components/admin/AdminHeader.jsx
+++ frontend/src/components/admin/AdminHeader.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import "../../styles/admin/AdminHeader.css";
+import logo from "../../assets/logo/Motobook.png";
 
 function AdminHeader() {
   return (
     <header className="admin-header">
+      <div className="header-logo">
+        <img src={logo} alt="Motobook Logo" />
+      </div>
       <h1>Admin Dashboard</h1>
+      <div className="header-spacer"></div>
     </header>
   );
 }
